82d17b9a616950b7e0dd06f825f808a2363b9687
KShervington <kyle.shervington@gmail.com>
2023-12-22 18:34:09 -0500

    vechain dependencies installed

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { keccak256 } from 'thor-devkit'
+import { DAppKit } from "@vechain/dapp-kit"
 
 export default function App() {
+
+  const dappKit = new DAppKit({
+    //Required
+    nodeUrl: 'https://sync-testnet.vechain.org/',
+    // Required if not connecting to the mainnet
+    genesis: 'test',
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Verifying functionality:</Text>
+      <Text>{keccak256('hello world').toString('hex')}</Text>
       <StatusBar style="auto" />
     </View>
   );
